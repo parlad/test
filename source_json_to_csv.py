ebe2ec0d528c2121823bd18e8c45625c60f114e4
Pralad Neupane <parladneupane@gmail.com>
2023-02-25 15:47:55 -0500

    updating last update
    updating last update

--- source_json_to_csv.py
+++ source_json_to_csv.py
@@ -2,23 +2,27 @@ import json
 import csv
 import chardet
 
-# Detect the encoding of the file
-with open('input_file.json', 'rb') as f:
-    result = chardet.detect(f.read())
+def extract_json_to_csv(input_file, output_file, columns_to_extract):
+    # Detect the encoding of the input file
+    with open(input_file, 'rb') as f:
+        result = chardet.detect(f.read())
 
-# Open the file with the detected encoding
-with open('input_file.json', encoding=result['encoding']) as f:
-    data = json.load(f)
+    # Open the input file with the detected encoding
+    with open(input_file, encoding=result['encoding']) as f:
+        data = json.load(f)
 
-# Define the columns to extract from the JSON data
+    # Create the output CSV file and write the column headers
+    with open(output_file, 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(columns_to_extract)
+
+        # Extract the specified columns from the JSON data and write them to the CSV file
+        for item in data:
+            row = [item[column] for column in columns_to_extract]
+            writer.writerow(row)
+    
+    print(f"CSV file '{output_file}' successfully created!")
+
+# Example usage of the function
 columns_to_extract = ['column1', 'column2', 'column3']
-
-# Create a CSV file and write the column headers
-with open('output_file.csv', 'w', newline='') as f:
-    writer = csv.writer(f)
-    writer.writerow(columns_to_extract)
-
-    # Extract the specified columns from the JSON data and write them to the CSV file
-    for item in data:
-        row = [item[column] for column in columns_to_extract]
-        writer.writerow(row)
+extract_json_to_csv('input_file.json', 'output_file.csv', columns_to_extract)
